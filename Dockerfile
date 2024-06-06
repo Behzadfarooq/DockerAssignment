def154a8d741040ba0cfd477d6a44e127586a9d9
Behzad Farooq <98178713+Behzadfarooq@users.noreply.github.com>
2024-06-06 19:28:47 +0530

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM openjdk:17-jdk-slim
+
+# Add a volume pointing to /tmp
+VOLUME /tmp
+
+# Copy the application's jar to the container
+COPY target/DockerAssignment-0.0.1-SNAPSHOT.jar app.jar
+
+# Run the jar file
+ENTRYPOINT ["java", "-jar", "/app.jar"]
---
+++ src/main/java/com/example/DockerAssignment/DockerAssignmentApplication.java
@@ -0,0 +1,14 @@
+package com.example.DockerAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerAssignmentApplication.class, args);
+	
+	}
+
+}
